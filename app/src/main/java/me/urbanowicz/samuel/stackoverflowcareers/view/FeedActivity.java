a7f915d7fc1b393d8e791e87d6cb7a0d1663fd26
samuel.urbanowicz <samuel.m.urbanowicz@gmail.com>
2016-02-28 00:16:36 +0100

    Update FeedActivity layout. Add java8 stream lib.

--- app/src/main/java/me/urbanowicz/samuel/stackoverflowcareers/view/FeedActivity.java
+++ app/src/main/java/me/urbanowicz/samuel/stackoverflowcareers/view/FeedActivity.java
@@ -3,36 +3,65 @@ package me.urbanowicz.samuel.stackoverflowcareers.view;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.annimon.stream.Optional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import me.urbanowicz.samuel.stackoverflowcareers.R;
+import me.urbanowicz.samuel.stackoverflowcareers.domain.JobPost;
 
 public class FeedActivity extends AppCompatActivity {
 
-    private RecyclerView feedRecyclerView;
+    private FeedRecyclerAdapter adapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
         setContentView(R.layout.activity_feed);
-        feedRecyclerView = (RecyclerView) findViewById(R.id.feedItemsRecyclerView);
+        RecyclerView feedRecyclerView = (RecyclerView) findViewById(R.id.feedItemsRecyclerView);
+        feedRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        feedRecyclerView.hasFixedSize();
+        adapter = new FeedRecyclerAdapter(Optional.<List<JobPost>>empty());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateFeed();
+    }
+
+    private void updateFeed() {
+        //todo
+        adapter.notifyDataSetChanged();
     }
 
     private static class FeedRecyclerAdapter extends RecyclerView.Adapter<FeedRecyclerAdapter.ViewHolder> {
+        private List<JobPost> jobPosts;
 
-        public FeedRecyclerAdapter() {
+        public FeedRecyclerAdapter(Optional<List<JobPost>> jobPosts) {
+            this.jobPosts = new ArrayList<>(jobPosts.get());
         }
 
         @Override
         public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            return null;
+            return new ViewHolder(
+                    LayoutInflater
+                            .from(parent.getContext())
+                            .inflate(R.layout.feed_item_view, parent, false)
+            );
         }
 
         @Override
         public void onBindViewHolder(ViewHolder holder, int position) {
-
+            ((TextView) holder.itemView).setText(jobPosts.get(position).getJobTitle());
         }
 
         @Override

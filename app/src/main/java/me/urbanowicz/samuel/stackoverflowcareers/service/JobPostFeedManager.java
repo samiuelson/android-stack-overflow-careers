8c19bd3b0d62ffc5a94e09e39c999e68a9c3d19b
samuel.urbanowicz <samuel.m.urbanowicz@gmail.com>
2016-03-20 23:36:46 +0100

    Clean up JobPostFeedManager logic. Add javadoc.

--- app/src/main/java/me/urbanowicz/samuel/stackoverflowcareers/service/JobPostFeedManager.java
+++ app/src/main/java/me/urbanowicz/samuel/stackoverflowcareers/service/JobPostFeedManager.java
@@ -10,10 +10,14 @@ import retrofit.Callback;
 import retrofit.Response;
 import retrofit.Retrofit;
 
+/**
+ * Class responsible for fetching, collecting and synthesis of JobPostsFeed.
+ * JobPostFeedManager instance is intended to live across activity life cycle changes /
+ * destroying-creating cycles.
+ */
 public class JobPostFeedManager {
 
     private static JobPostFeedManager INSTANCE;
-
     public static synchronized JobPostFeedManager getInstance() {
         if (INSTANCE == null) {
             INSTANCE = new JobPostFeedManager();
@@ -32,6 +36,7 @@ public class JobPostFeedManager {
     public void downloadFeedForNewSearch(Search search, JobPostsFeedManagerCallback callback) {
         this.currentPage = 0;
         this.search = search;
+        JobPostFeedManager.this.jobPosts.clear();
 
         fetchJobPosts(callback);
     }
@@ -55,19 +60,28 @@ public class JobPostFeedManager {
                     if (jobPosts.size() == 0) {
                         moreOfeersPossibility = MoreOfeersPossibility.IMPOSSIBLE;
                     }
-                    JobPostFeedManager.this.jobPosts.clear();
                     JobPostFeedManager.this.jobPosts.addAll(jobPosts);
-                    callback.onFeedUpdated(jobPosts, moreOfeersPossibility);
+                    callback.onFeedUpdated(JobPostFeedManager.this.jobPosts, moreOfeersPossibility);
                 } else {
                     // todo notify about error
+                    callback.onFeedUpdateError("");
                 }
             }
 
             @Override
             public void onFailure(Throwable t) {
                 // todo notify about error
+                callback.onFeedUpdateError("");
             }
         });
+    }
+
+    public Collection<JobPost> getCurrentJobPosts() {
+        return this.jobPosts;
+    }
+
+    public Search getCurrentSearch() {
+        return this.search;
     }
 
     public enum MoreOfeersPossibility {
@@ -75,9 +89,12 @@ public class JobPostFeedManager {
     }
 
     /**
-     * Interface providing callback method for a view
+     * Interface providing callback with a JobPost collection and some other info or error message
+     * used to get response back to the view module.
      */
     public interface JobPostsFeedManagerCallback {
         void onFeedUpdated(Collection<JobPost> jobPosts, MoreOfeersPossibility moreOfeersPossibility);
+
+        void onFeedUpdateError(String errorMessage);
     }
 }
